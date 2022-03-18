18d58f7d82f2a270e96f6850a3d509075f086b7b
Sky <skyrj50031@maricopa.edu>
2022-03-17 19:06:34 -0700

    Update index.js
    Added 7 more movies and directors
    Deleted methodOverride

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express'),
     morgan = require('morgan'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override');
+    bodyParser = require('body-parser');
 
 
 const app = express();
 
 app.use(morgan('common'));
 app.use(bodyParser.json());
-app.use(methodOverride());
+
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -16,33 +15,61 @@ app.use(bodyParser.urlencoded({
 
 
 
-// let top10Movies = [
-//     {
-//       title: 'Lucky Number Slevin',
-//       director: 'Paul McGuigan'
-//     },
-//     {
-//       title: 'The Gentlemen',
-//       director: 'Guy Ritchie'
-//     },
-//     {
-//       title: 'Batman: The Dark Night',
-//       director: 'Christopher Nolan'
-//     }
-//   ];
+let top10Movies = [
+    {
+        title: 'Lucky Number Slevin',
+        director: 'Paul McGuigan'
+    },
+    {
+        title: 'The Gentlemen',
+        director: 'Guy Ritchie'
+    },
+    {
+        title: 'Batman: The Dark Night',
+        director: 'Christopher Nolan'
+    },
+    {
+        title: 'Deadpool',
+        director: 'Tim Miller'
+    },
+    {
+        title: 'Deadpool 2',
+        director: 'David Leitch'
+    },
+    {
+        title: 'No Country For Old Men',
+        director: 'Ethan and Joel Coen'
+    },
+    {
+        title: 'Matrix',
+        director: 'Lana and Lilly Wachowski'
+    },
+    {
+        title: 'Knives Out',
+        director: 'Rian Johnson'
+    },
+    {
+        title: 'The Other Guys',
+        director: 'Adam McKay'
+    },
+    {
+        title: 'Live Free or Die Hard',
+        director: 'Len Wiseman'
+    }
+];
   
   // GET requests
-  app.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Welcome to ApiFlix!');
-  });
+});
   
-  app.get('/documentation', (req, res) => {                  
+app.get('/documentation', (req, res) => {                  
     res.sendFile('public/documentation.html', { root: __dirname });
-  });
+});
   
-  app.get('/movies', (req, res) => {
+app.get('/movies', (req, res) => {
     res.json(top10Movies);
-  });
+});
   
 // Serve static content from the public directory
 app.use(express.static('public'));
@@ -50,7 +77,7 @@ app.use(express.static('public'));
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
-  });
+});
 
 // listen for requests 8080
 app.listen(8080, () => {
