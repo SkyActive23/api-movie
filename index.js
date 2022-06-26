d9aa4865a7d2437e7b2455fc7c33f7e9650576f0
Sky <skyrj50031@maricopa.edu>
2022-06-26 15:00:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,22 @@ let allowedOrigins = [
   'https://movime.netlify.app',
   'Access-Control-Allow-Origin'
 ];
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          'The CORS policy for this application doesn’t allow access from origin ' +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    }
+  })
+);
 
 let auth = require('./auth.js')(app);
 
